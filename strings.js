d47e2c453c41cf32a37c69385d031b0095460986
pranavbatheja5 <20240802242@dypiu.ac.in>
2026-06-18 18:38:34 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,28 @@
+let name = "Pranav";
+console.log(name);
+let tx = "JavaScript";
+console.log(tx.length);
+let word = "Hello";
+console.log(word[0]);
+console.log(word[4]);
+let text = "JavaScript";
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+let text2 = "JavaScript";
+console.log(text2.slice(0, 4));
+console.log(text2.slice(4));
+let txt = "I love Java";
+console.log(txt.replace("Java", "JavaScript"));
+let first = "Hello";
+let second = " World";
+console.log(first.concat(second));
+let t = "JavaScript";
+console.log(t.includes("Script"));
+let arr = "Apple,Banana,Mango";
+console.log(arr.split(","));
+let st= "Hi ";
+console.log(t.repeat(3));
+let ar = "Hello";
+for (let char of ar) {
+    console.log(char);
+}
