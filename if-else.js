e94604b09bfa8f341c4b3b4f743a5a1e86ad391e
pranavbatheja5 <20240802242@dypiu.ac.in>
2026-06-18 11:12:45 +0530

    first commit

---
+++ if-else.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+let num = prompt("enter a number");
+if (num % 3 == 0) {
+    console.log(num, "is a multiple of 3")
+}
+    else
+    {
+        console.log(num, "is not a multiple of 3")
+    }
+
+
+    
