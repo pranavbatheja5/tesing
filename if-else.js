8f2c7a3e5297a43cb47562103a95251feb64e678
pranavbatheja5 <20240802242@dypiu.ac.in>
2026-06-18 11:14:31 +0530

    changed the number

--- if-else.js
+++ if-else.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")();
 let num = prompt("enter a number");
 if (num % 3 == 0) {
-    console.log(num, "is a multiple of 3")
+    console.log(num, "is a multiple of 4")
 }
     else
     {
